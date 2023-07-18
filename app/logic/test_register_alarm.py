cd9d73cd99f209196870340b913affed559c2294
Wanjuuuuu <makarong5798@gmail.com>
2023-07-18 14:31:15 +0900

    Separate logic of 'can register' cause inner conditions are individuals

--- app/logic/test_register_alarm.py
+++ app/logic/test_register_alarm.py
@@ -6,24 +6,27 @@ class RegisterAlarmCase(unittest.TestCase):
     STOCK = '카카오'
     USER = '유저'
 
-    def tearDown(self) -> None:
-        register_alarm.MOCK_REGISTERED_STOCKS.clear()
-        return
+    def test_kakao_is_not_registered_yet(self):
+        registered_stocks = []
+        already_registered = register_alarm.is_already_registered(self.STOCK, registered_stocks)
+        self.assertEqual(already_registered, False)
 
-    def test_can_register_stock_if_kakao_already_registered(self):
-        register_alarm.MOCK_REGISTERED_STOCKS.append(self.STOCK)
-        can_register = register_alarm.can_register_stock(self.STOCK, self.USER)
-        self.assertEqual(can_register, False)
-
-    def test_can_register_stock_if_already_registered_equal_to_limit(self):
-        for i in range(register_alarm.REGISTER_LIMIT_PER_USER):
-            register_alarm.MOCK_REGISTERED_STOCKS.append('')
-        can_register = register_alarm.can_register_stock(self.STOCK, self.USER)
-        self.assertEqual(can_register, False)
+    def test_kakao_is_already_registered(self):
+        registered_stocks = [self.STOCK]
+        already_registered = register_alarm.is_already_registered(self.STOCK, registered_stocks)
+        self.assertEqual(already_registered, True)
 
     def test_can_register_stock(self):
-        can_register = register_alarm.can_register_stock(self.STOCK, self.USER)
-        self.assertEqual(can_register, True)
+        registered_stocks = []
+        more_than_limit = register_alarm.is_registered_more_than_limit(registered_stocks)
+        self.assertEqual(more_than_limit, False)
+
+    def test_is_already_registered_equal_to_limit(self):
+        registered_stocks = []
+        for i in range(register_alarm.REGISTER_LIMIT_PER_USER):
+            registered_stocks.append('')
+        more_than_limit = register_alarm.is_registered_more_than_limit(registered_stocks)
+        self.assertEqual(more_than_limit, True)
 
 
 if __name__ == '__main__':
