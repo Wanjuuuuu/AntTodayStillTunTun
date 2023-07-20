e7345536adc28f06d528268a880b8d5d266c800c
Wanjuuuuu <makarong5798@gmail.com>
2023-07-20 16:50:10 +0900

    Add basic logic of parse message for registeration scenario

---
+++ app/message/parse_message.py
@@ -0,0 +1,72 @@
+from math import inf
+
+CANCEL_MESSAGE = '취소'
+RANGE_SEPARATOR = '~'
+
+
+class Message:
+    def __init__(self, content):
+        self.content = content
+
+
+class CancelMessage(Message):
+    def __init__(self):
+        super().__init__(CANCEL_MESSAGE)
+
+
+class InvalidMessage(Message):
+    def __init__(self):
+        super().__init__('')
+
+
+class StockNameMessage(Message):
+    def __init__(self, stock_name):
+        super().__init__(stock_name)
+
+
+class StockPriceRangeMessage(Message):
+    def __init__(self, lower_bound, upper_bound):
+        super().__init__('')
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+
+def parse_stock_name(message):
+    if CANCEL_MESSAGE in message:
+        return CancelMessage()
+    # TODO: validate stock name
+    return StockNameMessage(message)
+
+
+def parse_stock_price_range(message):
+    if CANCEL_MESSAGE in message:
+        return CancelMessage()
+    if RANGE_SEPARATOR not in message:
+        return InvalidMessage()
+
+    bounds = message.split(RANGE_SEPARATOR)
+    if len(bounds) != 2:
+        return InvalidMessage()
+
+    if bounds[0] == '' and bounds[1] == '':
+        return InvalidMessage()
+
+    lower_bound = parse_float(bounds[0])
+    upper_bound = parse_float(bounds[1])
+    if bounds[0] == '':
+        lower_bound = 0
+    if bounds[1] == '':
+        upper_bound = inf
+
+    if lower_bound < 0 or upper_bound < 0:
+        return InvalidMessage()
+    if lower_bound > upper_bound:
+        return InvalidMessage()
+    return StockPriceRangeMessage(lower_bound, upper_bound)
+
+
+def parse_float(s):
+    try:
+        return float(s)
+    except ValueError:
+        return -1
---
+++ app/message/test_parse_message.py
@@ -0,0 +1,86 @@
+import unittest
+
+from parse_message import *
+
+
+class ParseMessageCase(unittest.TestCase):
+    def test_parse_stock_name_if_cancel_message(self):
+        message = parse_stock_name(CANCEL_MESSAGE)
+        self.assertTrue(isinstance(message, CancelMessage))
+
+    def test_parse_stock_name_if_message_contains_cancel(self):
+        message = parse_stock_name('취소 카카오')
+        self.assertTrue(isinstance(message, CancelMessage))
+
+    def test_parse_stock_name(self):
+        message = parse_stock_name('카카오')
+        self.assertTrue(isinstance(message, StockNameMessage))
+
+    def test_parse_stock_price_range_if_cancel_message(self):
+        message = parse_stock_price_range(CANCEL_MESSAGE)
+        self.assertTrue(isinstance(message, CancelMessage))
+
+    def test_parse_stock_price_range_if_no_range_separator(self):
+        message = parse_stock_price_range('10-11')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_more_than_1_separator(self):
+        message = parse_stock_price_range('10~11~12')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_only_separator(self):
+        message = parse_stock_price_range('~')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_two_int_bounds(self):
+        message = parse_stock_price_range('10~12')
+        self.assertEqual(message.lower_bound, 10)
+        self.assertEqual(message.upper_bound, 12)
+
+    def test_parse_stock_price_range_if_two_float_bounds(self):
+        message = parse_stock_price_range('10.5~12.2')
+        self.assertEqual(message.lower_bound, 10.5)
+        self.assertEqual(message.upper_bound, 12.2)
+
+    def test_parse_stock_price_range_if_negative_upper_bound(self):
+        message = parse_stock_price_range('~-12.2')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_only_upper_bound(self):
+        message = parse_stock_price_range('~12.2')
+        self.assertEqual(message.lower_bound, 0)
+        self.assertEqual(message.upper_bound, 12.2)
+
+    def test_parse_stock_price_range_if_not_float_lower_bound(self):
+        message = parse_stock_price_range('카카오~')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_only_lower_bound(self):
+        message = parse_stock_price_range('10.5~')
+        self.assertEqual(message.lower_bound, 10.5)
+        self.assertEqual(message.upper_bound, inf)
+
+    def test_parse_stock_price_range_if_inf_upper_bound(self):
+        message = parse_stock_price_range('10.5~inf')
+        self.assertEqual(message.lower_bound, 10.5)
+        self.assertEqual(message.upper_bound, inf)
+
+    def test_parse_stock_price_range_if_no_bounds(self):
+        message = parse_stock_price_range(' ~ ')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_not_float_bound(self):
+        message = parse_stock_price_range('10~카카오')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_negative_bound(self):
+        message = parse_stock_price_range('-10~10')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+    def test_parse_stock_price_range_if_wrong_range(self):
+        message = parse_stock_price_range('inf~10')
+        self.assertTrue(isinstance(message, InvalidMessage))
+
+
+if __name__ == '__main__':
+    unittest.main()
